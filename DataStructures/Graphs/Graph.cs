092888039be2583d8ae636512d931a4f264f63e6
Darko <darko@matterless.com>
2022-11-13 20:21:53 +0100

    Added print list method to graph.cs

--- DataStructures/Graphs/Graph.cs
+++ DataStructures/Graphs/Graph.cs
@@ -1,13 +1,31 @@
 using System.Collections.Generic;
+using TOMICZ.Debugger;
 
 namespace TOMICZ.DSARunner
 {
     public class Graph
     {
-        public static void AddEddge(LinkedList<int>[] adjacencyList, int u, int v)
+        public void AddEdge(LinkedList<int>[] adjacencyList, int u, int v)
         {
             adjacencyList[u].AddLast(v);
             adjacencyList[v].AddLast(u);
+
+            //RuntimeConsole.Log($"Added vertex: {u} with edge to {v}");
+            //RuntimeConsole.Log($"Added vertex: {v} with edge to {u}");
+        }
+
+        public void PrintGraph(LinkedList<int>[] adjacencyList)
+        {
+            for (int i = 0; i < adjacencyList.Length; i++)
+            {
+                RuntimeConsole.Log("\n Adjacency list of vertex " + i);
+                RuntimeConsole.Header("Head");
+
+                foreach (var vertex in adjacencyList[i])
+                {
+                    RuntimeConsole.Log($" -> {vertex}");
+                }
+            }
         }
     }
 }
