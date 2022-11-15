ff8bf62bc2ba2d7236efcf3996186affaa380402
Darko <darko@matterless.com>
2022-11-15 04:24:10 +0100

    Added BFS search algorithm to the graph.cs

--- DataStructures/Graphs/Graph.cs
+++ DataStructures/Graphs/Graph.cs
@@ -1,29 +1,78 @@
 using System.Collections.Generic;
+using System.Linq;
 using TOMICZ.Debugger;
 
 namespace TOMICZ.DSARunner
 {
     public class Graph
     {
-        public void AddEdge(LinkedList<int>[] adjacencyList, int u, int v)
-        {
-            adjacencyList[u].AddLast(v);
-            adjacencyList[v].AddLast(u);
+        private int _numberOfVertices = 0;
+        private LinkedList<int>[] _adjecencyList;
 
-            //RuntimeConsole.Log($"Added vertex: {u} with edge to {v}");
-            //RuntimeConsole.Log($"Added vertex: {v} with edge to {u}");
+        public Graph(int numberOfVerticies)
+        {
+            _numberOfVertices = numberOfVerticies;
+
+            _adjecencyList = new LinkedList<int>[numberOfVerticies];
+
+            for (int i = 0; i < numberOfVerticies; i++)
+            {
+                _adjecencyList[i] = new LinkedList<int>();
+            }
         }
 
-        public void PrintGraph(LinkedList<int>[] adjacencyList)
+        public void AddEdge(int v, int e)
         {
-            for (int i = 0; i < adjacencyList.Length; i++)
+            _adjecencyList[v].AddLast(e);
+        }
+
+        public void PrintGraph()
+        {
+            for (int i = 0; i < _adjecencyList.Length; i++)
             {
                 RuntimeConsole.Log("\n Adjacency list of vertex " + i);
-                RuntimeConsole.Header("Head");
 
-                foreach (var vertex in adjacencyList[i])
+                foreach (var vertex in _adjecencyList[i])
                 {
                     RuntimeConsole.Log($" -> {vertex}");
+                }
+            }
+        }
+
+        public void Traverse(int initialVertext)
+        {
+            // Create array of visted vertices
+            bool[] visitedVerticies = new bool[_numberOfVertices];
+
+            // Set all vertices not visited
+            for (int i = 0; i < _numberOfVertices; i++)
+            {
+                visitedVerticies[i] = false;
+            }
+
+            // Create a queue of vertices
+            LinkedList<int> queue = new LinkedList<int>();
+
+            // Set initial vertext as visited on start
+            // Queue initial vertext to the list
+            visitedVerticies[initialVertext] = true;
+            queue.AddLast(initialVertext);
+
+            while (queue.Any())
+            {
+                initialVertext = queue.First();
+                RuntimeConsole.Log($"Vertext {initialVertext}");
+                queue.RemoveFirst();
+
+                LinkedList<int> list = _adjecencyList[initialVertext];
+
+                foreach (var val in list)
+                {
+                    if (!visitedVerticies[val])
+                    {
+                        visitedVerticies[val] = true;
+                        queue.AddLast(val);
+                    }
                 }
             }
         }
