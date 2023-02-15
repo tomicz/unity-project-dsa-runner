5471ae13dcb4aec027d1e7c5fbb668f89ff47d5f
Darko Tomic <tomiczdarko@gmail.com>
2023-02-15 23:55:28 +0100

    Added level order traversal (iterative)

--- DataStructures/Trees/BinaryTree.cs
+++ DataStructures/Trees/BinaryTree.cs
@@ -1,23 +1,31 @@
 using System;
+using System.Collections.Generic;
 using TOMICZ.Debugger;
 
 namespace TOMICZ.DSARunner.Trees
 {
     public class BinaryTree<T> where T : IComparable<T>
     {
-        public class TreeNode
+        public enum TraversalType
+        {
+            LevelOrder,
+            PreOrder,
+            InOrder,
+            PostOrder
+        }
+        public class Node
         {
             public T value;
 
-            public TreeNode left;
-            public TreeNode right;
+            public Node left;
+            public Node right;
 
-            public TreeNode(T value)
+            public Node(T value)
             {
                 this.value = value;
             }
 
-            public TreeNode(T value, TreeNode left, TreeNode right)
+            public Node(T value, Node left, Node right)
             {
                 this.value = value;
                 this.left = left;
@@ -25,26 +33,68 @@ namespace TOMICZ.DSARunner.Trees
             }
         }
 
-        public TreeNode root;
+        public Node root;
 
         public void Insert(T value)
         {
             if(root == null)
             {
-                root = new TreeNode(value);
+                root = new Node(value);
             }
 
             var current = root;
 
             if (value.CompareTo(root.value) < 0)
             {
-                current.left = new TreeNode(value);
+                current.left = new Node(value);
                 current = current.left;
             }
             else if(value.CompareTo(root.value) > 0)
             {
-                current.right = new TreeNode(value);
+                current.right = new Node(value);
                 current = current.right;
+            }
+        }
+
+        public void Traverse(TraversalType traversalType)
+        {
+            switch (traversalType)
+            {
+                case TraversalType.LevelOrder:
+                    LevelOrder();
+                    break;
+                case TraversalType.PreOrder:
+                    RuntimeConsole.Log("Preorder traversal not added.");
+                    break;
+                case TraversalType.InOrder:
+                    RuntimeConsole.Log("Inorder traversal not added.");
+                    break;
+                case TraversalType.PostOrder:
+                    RuntimeConsole.Log("Postorder traversal not added.");
+                    break;
+            }
+        }
+
+        private void LevelOrder()
+        {
+            Queue<Node> queue = new Queue<Node>();
+
+            Node tempNode = root;
+            queue.Enqueue(tempNode);
+
+            while (tempNode != null)
+            {
+                tempNode = queue.Dequeue();
+                RuntimeConsole.Log($"Node value: {tempNode.value}");
+
+                if (tempNode.left != null)
+                {
+                    queue.Enqueue(tempNode.left);
+                }
+                if (tempNode.right != null)
+                {
+                    queue.Enqueue(tempNode.right);
+                }
             }
         }
     }
